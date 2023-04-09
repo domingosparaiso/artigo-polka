caf461ad5fcc4e36434040c2395a18e983b33b44
Domingos Paraiso <domingos.paraiso@vixteam.com.br>
2023-04-08 22:46:53 -0300

    Gráfico do test3

--- artigo/test3/run4.py
+++ artigo/test3/run4.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
+import numpy
 
 run=4
 #csv output format: Type rate:
@@ -21,31 +22,53 @@ run=4
 # 15 errors_out 
 
 linkname=['MIA-SAO','CHI-AMS','MIA-CHI','MIA-CAL','','host2']
+colors=['#F6D32D','blue','red','green','','purple']
+
+plt.rc('font', size=16)
+plt.rc('axes', titlesize=16)
+plt.rc('axes', labelsize=16)
+plt.rc('xtick', labelsize=16)
+plt.rc('ytick', labelsize=16)
+plt.rc('legend', fontsize=10)
+plt.rc('figure', titlesize=16)
 for taxa in [ 100 ]:
 	print(f'Taxa: {taxa}mb')
-	for sample in range(1, 11):
-		for router in [ 2, 3, 4, 5, 7]:
+	y=[[],[],[],[],[]]
+	contr = 0
+	for router in [ 2, 3, 4, 5, 7]:
+		x=[]
+		for sample in range(1, 11):
 			arq=f'data/run{run}/{taxa}/{sample}/a{router}.csv'
 			f=open(arq)
 			linhas=f.readlines()
 			f.close()
 
-			y=[]
-			x=[]
 			cont=1
 			for dado in linhas:
-				x.append(cont)
-				cont=cont+1
-				b=dado.split(',')[3]
-				fb=float(b)/1024/1024*8
-				y.append(fb)
-			plt.yscale('linear')
-			plt.plot(x, y, label=linkname[router-2])
-		plt.xlabel('Tempo (s)')
-		plt.ylabel('Vazão (Mbps)') 
-		plt.legend(title='Link:', loc='lower left')
-		plt.title('Agregação de fluxos com uso de políticas')
-		print(f'Sample {sample}: OK')
-		plt.savefig(f'results/run{run}/{taxa}-{sample}.png')
-		plt.clf()
+				if cont < 96:
+					if sample == 1:
+						x.append(cont)
+						y[contr].append([])
+					b=dado.split(',')[3]
+					fb=float(b)/1024/1024*8
+					y[contr][cont-1].append(fb)
+					cont=cont+1
+		m=[]
+#		print('contr:',contr)
+		for cont in range(0,len(y[contr])):
+			m.append(numpy.mean(y[contr][cont]))
+		plt.plot(x, m, linewidth=1, marker='o', markersize=2, label=linkname[router-2], color=colors[router-2])
+		#plt.plot(x, m, label=linkname[router-2], color=colors[router-2])
+		contr = contr+1
+
+	plt.yscale('linear')
+	plt.xlabel('Tempo (s)')
+	plt.ylabel('Vazão (Mbps)') 
+	plt.legend(title='Link:', bbox_to_anchor=(1.05, 1.0), loc='upper left')
+	plt.tight_layout()
+	#plt.legend(title='Link:', loc='lower left')
+	plt.title('Agregação de fluxos com uso de políticas')
+	print(f'Sample {sample}: OK')
+	plt.savefig(f'results/run{run}/{taxa}.png')
+	plt.clf()
 
